c91c045c3ea90247dc751d393b901c4510cf507d
Marc Brittain <mwb@iastate.edu>
2020-11-18 10:12:57 -0600

    add scripts

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Passive-OpenSky-Collection
-Passively collect OpenSky data in python.
+
+Python 3.x repo for passively collecting data from OpenSky using the OpenSky API.
+
+## Requirements
+
+### OpenSky Account
+
+You will need to obtain an OpenSky account for accessing their data to full capacity. Account rresgistration can be found here: [Register](https://opensky-network.org/index.php?option=com_users&view=registration).
+
+### OpenSky API
+
+The OpenSky API python package will need to be cloned and installed from the [OpenSky Github](https://github.com/openskynetwork/opensky-api). Follow the recommended **pip** installation.
+
+## Usage
+
+`collectData.py` is simple script for passively collecting latitude and longitude values and saving the results in numpy arrays. While only latitude and longitude are being collected, this script can be easily extended to collect all of the return fields as listed here: [Returns](https://opensky-network.org/apidoc/python.html#opensky_api.StateVector).
+
+The run script is as follows:
+```python
+python collectData.py --username your_username --password your_password
+                      --bbox min_latitude max_latitude min_longitude max_longitude
+
+```
+
+This script will begin collecting data approximately every 5 seconds from OpenSky for the specified bounding box. Data will be collected for approximately 1 hour and then saved to a created data directory with the following format: `data/<year>/<month>/<day>/<hour>.npy`. `.npy` files are in binary format which makes them more memory efficient as compared to `.csv` files. In addition, collecting data in 1 hour increments ensures that the python script does not consume too much memory. In the event that a file aleady exists for that hour, then the minute will also be added to the file name: `/<hour>_<min>.npy`.
+
+Please note that the data corresponding to `<hour>` will be all of the data collected from the point of save to 1 hour in the past. Depending on when the script is started, this time may be over two hours. For example, if the script is started at 1430, the first data will be saved as `/13.npy` and incorporate the data from **1430 to 1330**.
---
+++ collectData.py
@@ -0,0 +1,110 @@
+from opensky_api import OpenSkyApi
+import time
+import os
+import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--username", help="OpenSky Username")
+parser.add_argument("--password", help="OpenSky Password")
+parser.add_argument('--bbox', nargs='+', type=float)
+args = parser.parse_args()
+
+
+BBOX = tuple(args.bbox)
+USERNAME = args.username
+PASSWORD = args.password
+
+# collecting lat/lon data
+latitudes = []
+longitudes = []
+
+# hour represents the hour since the python script began, not the
+# hour of the day
+hour = 0
+
+# counting the number of states collecting
+counter = 0
+
+while True:
+    start = time.time()
+    api = OpenSkyApi(username=USERNAME,password=PASSWORD)
+
+    try:
+        states = api.get_states(time_secs=time.time(),bbox=BBOX)
+    except:
+        ##OpenSky data is updated every 5 seconds so sleep for 5
+        time.sleep(5)
+        continue
+
+
+    # collecting lat/lon data
+    try:
+
+        for s in states.states:
+            latitudes.append(s.latitude)
+            longitudes.append(s.longitude)
+
+
+    # this will occur if there is no data in states
+    except:
+        timeinfo = time.gmtime()
+
+        day = timeinfo.tm_mday
+        month = timeinfo.tm_mon
+        year = timeinfo.tm_year
+        hr = timeinfo.tm_hour
+        minute = timeinfo.tm_min
+        sec = timeinfo.tm_sec
+
+        print("Error collecting data for {}/{}/{}/{}:{}".format(year,month,day,hr,minute,sec))
+        end = time.time()
+
+        if not abs(end-start) > 5:
+            time.sleep(5-abs(end-start))
+        continue
+
+
+
+    end = time.time()
+
+    # We only want to collect data at most every 5 seconds
+    if not abs(end-start) > 5:
+        time.sleep(5-abs(end-start))
+
+    counter += 1
+
+
+    ## ~ 1 hour worth of data: 3600 seconds / 5 seconds data collection
+    ## This could be updated based on the start time.time() and the current time.time()
+    ## to make this exactly 1 hour instead of ~ 1 hour
+    if counter == 720:
+
+
+        timeinfo = time.gmtime()
+
+        day = timeinfo.tm_mday
+        month = timeinfo.tm_mon
+        year = timeinfo.tm_year
+        hr = timeinfo.tm_hour
+        minute = timeinfo.tm_min
+        sec = timeinfo.tm_sec
+
+        # convert data to a matrix of (N, 2)
+        latlon = np.vstack([latitudes,longitudes]).T
+        latitudes = []
+        longitudes = []
+
+        # save data
+        os.makedirs("data/{}/{}/{}/".format(year,month,day),exist_ok=True)
+
+        if not os.path.isfile('data/{}/{}/{}/{}.npy'.format(year,month,day,hr)):
+            np.save('data/{}/{}/{}/{}.npy'.format(year,month,day,hr),latlon)
+
+        else:
+            np.save('data/{}/{}/{}/{}_{}.npy'.format(year,month,day,hr,minute),latlon)
+
+        hour += 1
+        counter = 0
+
+        print("Hours Processed: {}".format(hour))
